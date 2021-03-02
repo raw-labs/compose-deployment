50ade8f83a60e8ca0480f588bb8abe02d7fe8afc
Lionel Sambuc <lionel.sambuc@epfl.ch>
2021-03-02 09:39:43 +0100

    Keep Spark driver port private by default.

--- settings.default.sh
+++ settings.default.sh
@@ -14,11 +14,16 @@
 # be relative to the host because it will be used to launch the container
 # with the driver using the Docker daemon that runs on the host.
 
+# Uncomment, or copy this to your local settings to expose the Spark driver.
+#: ${SPARK_PORT:="-Draw.executor.spark.docker-driver.ports.0=4040:4040"}
+: ${SPARK_PORT:=""}
+
 # Spark is starting the containers for the workers by itself, outside of
 # docker-compose, which means we have to prefix ourselves in the same way
 # the docker volume names used, for example for the raw cache. The prefix
 # to use is `${COMPOSE_PROJECT_NAME}_`.
 : ${EXECUTOR_JAVA_OPTS:="
+    ${SPARK_PORT}
     -Draw.executor.spark.docker-driver.volumes.0=$(pwd)/conf/raw-driver/logback.xml:/opt/docker/conf/logback.xml:ro
     -Draw.executor.spark.docker-driver.volumes.1=${COMPOSE_PROJECT_NAME}_cache:/var/tmp/rawcache
     -Draw.executor.spark.docker-driver.volumes.2=$(pwd)/conf/raw-driver/core-site.xml:/opt/docker/conf/core-site.xml:ro
